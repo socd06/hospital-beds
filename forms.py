38b2ed73c582d4f30a1c55160082188ba92c9610
Carlos <socd06@gmail.com>
2020-08-19 23:19:34 -0400

    chore: update forms.py

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Optional, Length
+
+class MapSeachForm(FlaskForm):
+    #STATE_NAME feature from dataset
+    # DataRequired means it can't be empty
+    state_name = StringField('STATE_NAME',
+                    validators=[Optional(), Length(min=4, max=20)])
+    #HQ_CITY feature from dataset
+    city_name =  StringField('HQ_CITY',
+                    validators=[Optional(), Length(min=4, max=20)])
+    #HQ_ZIP_CODE
+    zip_code = StringField('HQ_ZIP_CODE',
+                    validators=[Optional(), Length(min=5, max=5)])
+    #COUNTY_NAME
+    county_name = StringField('COUNTY_NAME',
+                    validators=[Optional(), Length(min=4, max=20)])
+
+    submit = SubmitField("Search")
+
+#HOSPITAL_TYPE # Not sure about this one
