618599a97c6d2e92184251cc50856dca49c4e713
Carlos <socd06@gmail.com>
2020-08-21 00:11:17 -0400

    chore: make state search form required field

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import Optional, Length
+from wtforms.validators import Required, Optional, Length
 
 class MapSeachForm(FlaskForm):
     #STATE_NAME feature from dataset
     # DataRequired means it can't be empty
     state_name = StringField('STATE_NAME',
-                    validators=[Optional(), Length(min=4, max=20)])
+                    validators=[Required(), Length(min=4, max=20)])
     #HQ_CITY feature from dataset
     city_name =  StringField('HQ_CITY',
                     validators=[Optional(), Length(min=4, max=20)])
