f7feeec48acdc9442116f169ec2a91d38b110495
Carlos <socd06@gmail.com>
2020-08-21 20:35:51 -0400

    docs: add comments to forms and make all fields optional

--- forms.py
+++ forms.py
@@ -6,14 +6,16 @@ class MapSeachForm(FlaskForm):
     #STATE_NAME feature from dataset
     # DataRequired means it can't be empty
     state_name = StringField('STATE_NAME',
-                    validators=[Required(), Length(min=4, max=20)])
+                    validators=[Optional(), Length(min=4, max=20)])
     #HQ_CITY feature from dataset
     city_name =  StringField('HQ_CITY',
                     validators=[Optional(), Length(min=4, max=20)])
     #HQ_ZIP_CODE
+    # Currently not used
     zip_code = StringField('HQ_ZIP_CODE',
                     validators=[Optional(), Length(min=5, max=5)])
     #COUNTY_NAME
+    # Not implemented yet
     county_name = StringField('COUNTY_NAME',
                     validators=[Optional(), Length(min=4, max=20)])
 
