cb2ff5c5a3386dc73cdb3f1123daa662642ea44f
Carlos Salgado <socd06@gmail.com>
2020-08-25 23:42:56 -0400

    feat: add regular expressions to process input text

--- backend.py
+++ backend.py
@@ -2,6 +2,53 @@ import pandas as pd
 import folium
 from folium import plugins
 import math
+import re
+
+def process_input(user_input):
+    '''
+    Returns the processed user input string where the first letter of every
+    word is always a capital letter so it matches the dataset
+
+    Parameters:
+
+    user_input (string): user input location search
+    '''
+
+    if len(user_input) >3:
+
+        lower_input = user_input.lower()
+
+        input_list = re.split(r'\s',lower_input)
+
+        output_str = ""
+
+        # White spaces should be 1 less than how many words
+        ws_qty = len(input_list) - 1
+        ws_count = 0
+
+        if len(input_list) > 1:
+
+            for elem in input_list:
+
+                # Make capital only the first letter of every word
+                output_str += elem[0].upper()
+
+                # Concatenate the rest of the word
+                output_str += elem[1::]
+
+                # Add a whitepace to the string if not in the last word of the list
+                if ws_count < ws_qty:
+                    output_str += " "
+                    ws_count+= 1
+        else:
+            output_str = lower_input[0].upper() + lower_input[1::]
+
+        return output_str
+
+    else:
+        return None
+
+
 
 def data_filter(dataframe_path, features):
     '''
